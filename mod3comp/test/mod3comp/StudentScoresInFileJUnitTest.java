3c60a583c44b46863c33fd4bb64619ca5532d12d
Kevin Bell <bellKevin@pm.me>
2022-09-26 14:46:50 -0600

    Update StudentScoresInFileJUnitTest.java

--- mod3comp/test/mod3comp/StudentScoresInFileJUnitTest.java
+++ mod3comp/test/mod3comp/StudentScoresInFileJUnitTest.java
@@ -4,7 +4,7 @@
  *
  * Objects read from files, calculate & display lowest, highest, & average score
  **************************************************************************** */
-package mod3comp;
+package mod3competency;
 
 import org.junit.Test;
 import org.junit.Assert.*;
@@ -12,6 +12,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import java.util.Arrays;
 import java.util.Collection;
+import mod3comp.Gradebook;
+import mod3comp.Student;
 import org.junit.Assert;
 
 @RunWith(Parameterized.class)
@@ -28,17 +30,22 @@ public class StudentScoresInFileJUnitTest {
     @Parameterized.Parameter(3)
     public Student[] students;
 
-    Student[] studentList = new Student[]{new Student("Kevin", 100.0), new Student("George", 0.0)};
-    Student[] studentList2 = new Student[]{new Student("Anna", 99), new Student("Ben", 51)};
-
     @Parameterized.Parameters
-    public Collection<Object[]> data() {
+    public static Collection<Object[]> data() {
+        Student[] studentList = new Student[]{new Student("Anna",
+            95.2), new Student("Ben", 89.1), new Student("Carl", 90.0),
+            new Student("Debbie", 92.4), new Student("Emily", 88.0)};
+        Student[] studentList2 = new Student[]{new Student("Anna", 21),
+            new Student("Ben", 17)};
         Object[][] data = new Object[][]{
-            {0, 100, 50, studentList},
-            {1, 99, 51, studentList2},
-            {2, 98, 74, new Student[]{new Student("Anna", 99), new Student("Ben", 51)}},
-            {3, 97, 73, new Student[]{new Student("Anna", 99), new Student("Ben", 51)}},
-            {4, 96, 72, new Student[]{new Student("Anna", 99), new Student("Ben", 51)}}
+            {88.0, 95.2, 90.9400000000000001, studentList},
+            {17, 21, 19, studentList2},
+            {75, 99, 87, new Student[]{new Student("Juan", 99),
+                new Student("Lupe", 75)}},
+            {51, 98, 74.5, new Student[]{new Student("Sean", 98),
+                new Student("Hannity", 51)}},
+            {23, 97, 60, new Student[]{new Student("Tucker", 97),
+                new Student("Carlson", 23)}}
         };
         return Arrays.asList(data);
     }
@@ -63,10 +70,9 @@ public class StudentScoresInFileJUnitTest {
 
     @Test
     public void testAverageScore() {
-        
-        Gradebook scores = new Gradebook(sumOfScores, studentCount);
-        double actual = scores.getAverageScore(sumOfScores / studentCount);
-        double result = sumOfScores / studentCount;
+        Gradebook scores = new Gradebook(students);
+        double actual = scores.getAverageScore();
+        double result = averageScore;
         Assert.assertEquals(result, actual, 0.01);
     } // end testAverageScore method
 //******************************************************************************    
