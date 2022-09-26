cb18acdb805f0d4421e1b9a23627912952b8e049
Kevin Bell <bellKevin@pm.me>
2022-09-26 12:42:39 -0600

    Update Student.java

--- mod3comp/src/mod3comp/Student.java
+++ mod3comp/src/mod3comp/Student.java
@@ -8,14 +8,11 @@ package mod3comp;
 
 import java.io.*;
 
-/**
- *
- * @author 4800590195
- */
 public class Student implements Serializable {
 
     double score;
     String name;
+//******************************************************************************
 
     public Student(String name, double score) {
         this.name = name;
@@ -25,13 +22,17 @@ public class Student implements Serializable {
 
     public String getName() {
         return this.name;
-    }
+    } // end getName method
+//******************************************************************************
 
     public double getScore() {
         return this.score;
-    }
+    } // end getScore method
+//******************************************************************************
+
     @Override
-    public String toString(){
-        return "i don't know what I'm doing";
-    }
+    public String toString() {
+        return name + "\n" + this.score;
+    } // end String override method
+//******************************************************************************    
 } // end Student class
